932592c527a6b3d99abc22389c8981924c2b8179
vladyslavbabenk0 <thevladman17@gmail.com>
2023-12-03 22:59:59 +0100

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -6,6 +6,7 @@ import './index.css';
 import { Provider } from 'react-redux';
 import { store } from './redux/store.js';
 
+
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <BrowserRouter>
