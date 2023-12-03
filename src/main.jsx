1614a79df6abb0ac4e00a741b20bf9c27a574c74
vladyslavbabenk0 <thevladman17@gmail.com>
2023-12-03 22:53:19 +0100

    fix

--- src/main.jsx
+++ src/main.jsx
@@ -8,7 +8,7 @@ import { store } from './redux/store.js';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <BrowserRouter basename="/CarSharing">
+    <BrowserRouter>
       <Provider store={store}>
         <App />
       </Provider>
